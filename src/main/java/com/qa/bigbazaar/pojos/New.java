c61baa97a04ab04c196613e917d1c0b5013d7be5
Ayesha Yusuf <450520@FutureGroup.net>
2021-04-23 14:49:06 +0530

    Adding the getters/setters

--- src/main/java/com/qa/bigbazaar/pojos/New.java
+++ src/main/java/com/qa/bigbazaar/pojos/New.java
@@ -1,7 +1,25 @@
 package com.qa.bigbazaar.pojos;
 
 public class New {
-	
-	//Adding this new pojo class
+
+	// Adding this new pojo class
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 }
